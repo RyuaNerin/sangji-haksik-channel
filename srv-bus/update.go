a34611e4ca937956dab1ef9456d1ff76f0ef9218
RyuaNerin <admin@ryuar.in>
2020-06-30 22:14:59 +0900

    no-log

--- srv-bus/update.go
+++ srv-bus/update.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"sangjihaksik/share"
 	"sort"
@@ -224,8 +223,6 @@ func (ri *routeInfo) update(w *sync.WaitGroup) {
 			},
 		},
 	}
-
-	log.Println(body)
 
 	err := jsoniter.NewEncoder(&ri.skillResponseBodyBuffer).Encode(&skillResponse)
 	if err != nil {
