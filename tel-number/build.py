6546580e351b29b67eb815eee6d402830d6f2e93
RyuaNerin <admin@ryuar.in>
2020-07-11 16:39:58 +0900

    시설 / 부서 정보 추가

--- tel-number/build.py
+++ tel-number/build.py
@@ -3,8 +3,9 @@
 import csv
 import re
 
-with open("build.csv", "w", encoding="utf-8") as _fw:
+with open("build.csv", "w", encoding="utf-8", newline="") as _fw:
     _fw.truncate(0)
+    _fw.write("\ufeff")
 
     writer = csv.writer(_fw)
     writer.writerow(
@@ -24,35 +25,31 @@ with open("build.csv", "w", encoding="utf-8") as _fw:
 
     row_number = 0
 
-    with open("department.csv", "r", encoding="utf-8") as _fr:
-        keyword_dup = set()
+    ###########################################################################
 
+    with open("department.csv", "r", encoding="utf-8") as _fr:
+        f = True
         for row in csv.reader(_fr, delimiter="\t"):
-            if row[0] == "대학":
+            if f:
+                f = False
                 continue
 
             category2 = row[0]
             category3 = row[1]
             question_list = [row[1]]
-            answer = "{0}\n{1}\n{2}".format(category2, category3, row[2])
+            answer = "<{0}>\n{1}\n{2}".format(category2, category3, row[2])
             url = row[3]
 
-            question_list.extend([keyword for keyword in row[4:] if len(keyword) > 0])
+            question_list.extend(row[4:])
 
-            for question in set(question_list):
-                if question in keyword_dup:
-                    raise Exception("Keyword {} is Duplicated".format(question))
-                keyword_dup.add(question)
-
-            tm = re.match(r"^\d\d\d\-(\d\d\d)\-(\d\d\d\d)$", row[2])
+            tm = re.match(r"^\d\d\d\-(\d\d\d\-(\d\d\d\d))$", row[2])
             if tm is not None:
-                question_list.extend(
-                    [row[2], "{0}-{1}".format(tm[1], tm[2]), "{0}".format(tm[2]),]
-                )
+                question_list.extend([row[2], tm[1], tm[2]])
 
-            for question in set(question_list):
+            for question in [
+                x for x in set(question_list) if x is not None and len(x) > 0
+            ]:
                 row_number += 1
-
                 writer.writerow(
                     [
                         row_number,
@@ -68,3 +65,53 @@ with open("build.csv", "w", encoding="utf-8") as _fw:
                     ]
                 )
 
+    ###########################################################################
+
+    with open("infomation.csv", "r", encoding="utf-8") as _fr:
+        f = True
+        for row in csv.reader(_fr, delimiter="\t"):
+            if f:
+                f = False
+                continue
+
+            if len([p for p in row[2:7] if p is not None]) == 0:
+                continue
+
+            category2 = row[0]
+            category3 = row[1]
+            question_list = [row[1]]
+            answer = "<{0}>\n{1}\n{2}\n\n{3}".format(
+                category2,
+                category3,
+                "\n".join(
+                    [tel for tel in row[3:7] if tel is not None and len(tel) > 0]
+                ).strip(),
+                row[2],
+            ).strip()
+            url = row[8]
+
+            question_list.extend(row[9:])
+
+            for tel in row[3:7]:
+                tm = re.match(r"^\d\d\d\-(\d\d\d\-(\d\d\d\d))$", tel)
+                if tm is not None:
+                    question_list.extend([tel, tm[1], tm[2]])
+
+            for question in [
+                x for x in set(question_list) if x is not None and len(x) > 0
+            ]:
+                row_number += 1
+                writer.writerow(
+                    [
+                        row_number,
+                        "부서 및 시설물",
+                        category2,
+                        category3,
+                        None,
+                        None,
+                        question,
+                        answer,
+                        url,
+                        None,
+                    ]
+                )
